2313ac5f4cc4425053800069b525ad7e21471b7d
Telliotword <telliotword@gmail.com>
2016-02-02 10:02:34 -0600

    Made It  Hot Cross Buns

--- arduino_traffic_light/arduino_traffic_light.ino
+++ arduino_traffic_light/arduino_traffic_light.ino
@@ -3,78 +3,121 @@
 #define   LED_1   6
 #define   LED_2   10
 #define   LED_3   11
+#define   SPEAKER 5
+#define NOTE_G1  49
+#define NOTE_B1  62
+#define NOTE_A1  55
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(LED_1, OUTPUT);
   pinMode(LED_2, OUTPUT);
   pinMode(LED_3, OUTPUT);
+  pinMode (SPEAKER, OUTPUT); 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_1, HIGH); 
-  delay(1500);
-  digitalWrite(LED_1, LOW);
-  delay(250);
-  digitalWrite(LED_2, HIGH);
-  delay(250);
-  digitalWrite(LED_2, LOW);
-  delay(250);
-  digitalWrite(LED_3, HIGH);
-  delay(250);
-  digitalWrite(LED_3, LOW);
-  delay(250);
   digitalWrite(LED_1, HIGH);
-  delay(250);
-  digitalWrite(LED_1, LOW);
-  delay(250);
-  digitalWrite(LED_2, HIGH);
-  delay(250);
-  digitalWrite(LED_2, LOW);
-  delay(250);
-  digitalWrite(LED_3, HIGH);
-  delay(250);
-  digitalWrite(LED_3, LOW);
-  delay(250);  
-  digitalWrite(LED_1,HIGH);
-  delay(250);
-  digitalWrite(LED_1, LOW);
-  delay(250);
-  digitalWrite(LED_2, HIGH);
-  delay(250);
-  digitalWrite(LED_2, LOW);
+  tone(SPEAKER, NOTE_B1); 
   delay(500);
+  noTone(SPEAKER);
+  digitalWrite(LED_1, LOW);
+  delay(150);
+  digitalWrite(LED_2, HIGH);
+  tone(SPEAKER, NOTE_A1); 
+  delay(500);
+  noTone(SPEAKER);
+  digitalWrite(LED_2, LOW);
+  delay(150);
   digitalWrite(LED_3, HIGH);
-  delay(750);
+  tone(SPEAKER, NOTE_G1); 
+  delay(500);
+  noTone(SPEAKER);
   digitalWrite(LED_3, LOW);
-  delay(250);
+  delay(150);
   digitalWrite(LED_1, HIGH);
-  delay(250);
+  tone(SPEAKER, NOTE_B1); 
+  delay(500);
+  noTone(SPEAKER);
   digitalWrite(LED_1, LOW);
-  delay(250);
+  delay(150);
   digitalWrite(LED_2, HIGH);
-  delay(250);
+  tone(SPEAKER, NOTE_A1); 
+  delay(500);
+  noTone(SPEAKER);
   digitalWrite(LED_2, LOW);
-  delay(250);
+  delay(150);
   digitalWrite(LED_3, HIGH);
-  delay(250);
+  tone(SPEAKER, NOTE_G1); 
+  delay(500);
+  noTone(SPEAKER);
   digitalWrite(LED_3, LOW);
-  delay(250);  
-  digitalWrite(LED_1,HIGH);
-  delay(250);
+  delay(150); 
+  digitalWrite(LED_3,HIGH);
+  tone(SPEAKER, NOTE_G1); 
+  delay(150);
+  noTone(SPEAKER);
+  digitalWrite(LED_3, LOW);
+  delay(100);
+  digitalWrite(LED_3, HIGH);
+  tone(SPEAKER, NOTE_G1); 
+  delay(150);
+  noTone(SPEAKER);
+  digitalWrite(LED_3, LOW);
+  delay(100);
+  digitalWrite(LED_3, HIGH);
+  tone(SPEAKER, NOTE_G1); 
+  delay(150);
+  noTone(SPEAKER);
+  digitalWrite(LED_3, LOW);
+  delay(100);
+  digitalWrite(LED_3, HIGH);
+  tone(SPEAKER, NOTE_G1);  
+  delay(150);
+  noTone(SPEAKER);
+  digitalWrite(LED_3, LOW);
+  delay(100);
+  digitalWrite(LED_2, HIGH);
+  tone(SPEAKER, NOTE_A1); 
+  delay(150);
+  noTone(SPEAKER);
+  digitalWrite(LED_2, LOW);
+  delay(100);
+  digitalWrite(LED_2, HIGH);
+  tone(SPEAKER, NOTE_A1); 
+  delay(150);
+  noTone(SPEAKER);
+  digitalWrite(LED_2, LOW);
+  delay(100);
+  digitalWrite(LED_2, HIGH);
+  tone(SPEAKER, NOTE_A1); 
+  delay(150);
+  noTone(SPEAKER);
+  digitalWrite(LED_2, LOW);
+  delay(100);  
+  digitalWrite(LED_2,HIGH);
+  tone(SPEAKER, NOTE_A1); 
+  delay(150);
+  noTone(SPEAKER);
+  digitalWrite(LED_2, LOW);
+  delay(100);
+  digitalWrite(LED_1, HIGH);
+  tone(SPEAKER, NOTE_B1); 
+  delay(500);
+  noTone(SPEAKER);
   digitalWrite(LED_1, LOW);
-  delay(250);
+  delay(150);
   digitalWrite(LED_2, HIGH);
-  delay(250);
+  tone(SPEAKER, NOTE_A1); 
+  delay(500);
+  noTone(SPEAKER);
   digitalWrite(LED_2, LOW);
-  delay(250);
+  delay(150);
   digitalWrite(LED_3, HIGH);
-  delay(750);
+  tone(SPEAKER, NOTE_G1); 
+  delay(500);
+  noTone(SPEAKER);
   digitalWrite(LED_3, LOW);
-  delay(250);
-
-
-
- 
+  delay(150);  
 }
